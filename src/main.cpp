100967f54071ecad570456ca68100d27f1f9feb3
Josh Charpentier <81543042+jcharpi@users.noreply.github.com>
2026-05-24 14:00:17 -0500

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <format>
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -7,4 +8,7 @@ int main() {
   std::cerr << std::unitbuf;
 
   std::cout << "$ ";
+  std::string command;
+  std::getline(std::cin, command);
+  std::cout << std::format("{}: command not found\n", command);
 }
