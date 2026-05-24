fd4496ed7c08267242e6d4f03a418ef1d7c538bf
Josh Charpentier <81543042+jcharpi@users.noreply.github.com>
2026-05-24 16:02:01 -0500

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,69 @@
+#include <cstdlib>
 #include <iostream>
 #include <string>
 #include <format>
 #include <vector>
+#include <algorithm>
+#include <sstream>
+#include <unistd.h>
+
+#ifdef _WIN32 
+constexpr char PATH_DELIMITER = ';';
+#else
+constexpr char PATH_DELIMITER = ':';
+#endif
+
+bool isFileExecutable(const std::string& path) {
+  return access(path.c_str(), X_OK) == 0;
+}
+
+std::string executableInPATH(const std::string& fileName) {
+  const char* directories = std::getenv("PATH");
+  if (!directories) return "";
+
+  std::stringstream ss_directories(directories);
+  std::string directory;
+  while (std::getline(ss_directories, directory, PATH_DELIMITER)) {
+    std::string fullPath = directory + "/" + fileName;
+    if (isFileExecutable(fullPath)) {
+      return fullPath;
+    }
+  }
+  return "";
+}
 
 int main() {
-  // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+
+  const std::vector<std::string> builtins = {"exit", "echo", "type"};
+
+  std::string input;
   while (true) {
     std::cout << "$ ";
-    std::string input;
-    std::getline(std::cin, input);
+    if (!std::getline(std::cin, input)) break;
 
-    std::vector<std::string> builtins = {"exit", "echo", "type"};
+    std::string cmd = input.substr(0, input.find(" "));
+    std::string args = input.size() > cmd.size() ? input.substr(cmd.size() + 1) : "";
 
-    if (input == "exit") {
+    if (cmd == "exit") {
       break;
-    } else if (input.substr(0, 4) == "echo") {
-      std::cout << input.substr(5, input.size()) << "\n";
-    } else if (input.substr(0, 4) == "type") {
-      std::string command = input.substr(5, input.size());
-      if (std::find(builtins.begin(), builtins.end(), command) != builtins.end()) {
-        std::cout << command << " is a shell builtin\n";
-      } else {
-        std::cout << command << ": not found\n";
+    } else if (cmd == "echo") {
+      std::cout << args << "\n";
+    } else if (cmd == "type") {
+      // Builtin
+      if (std::find(builtins.begin(), builtins.end(), args) != builtins.end()) {
+        std::cout << args << " is a shell builtin\n";
+      } else { // Executable
+        std::string executablePath = executableInPATH(args);
+        if (!executablePath.empty()) {
+          std::cout << cmd << " is " << executablePath << "\n";
+        } else {
+          std::cout << cmd << ": not found\n";
+        }
       }
-    } else {
-      std::cout << std::format("{}: command not found\n", input);
+    } else { // Unrecognized
+      std::cout << std::format("{}: command not found\n", cmd);
     }
   }
 }
