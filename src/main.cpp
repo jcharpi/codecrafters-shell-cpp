c149a68ad6e6f72562a397fc4ec60a17db0c816f
Josh Charpentier <81543042+jcharpi@users.noreply.github.com>
2026-05-24 14:39:37 -0500

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <format>
+#include <vector>
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -11,10 +12,19 @@ int main() {
     std::string input;
     std::getline(std::cin, input);
 
+    std::vector<std::string> builtins = {"exit", "echo", "type"};
+
     if (input == "exit") {
       break;
     } else if (input.substr(0, 4) == "echo") {
       std::cout << input.substr(5, input.size()) << "\n";
+    } else if (input.substr(0, 4) == "type") {
+      std::string command = input.substr(5, input.size());
+      if (std::find(builtins.begin(), builtins.end(), command) != builtins.end()) {
+        std::cout << command << " is a shell builtin\n";
+      } else {
+        std::cout << command << ": not found\n";
+      }
     } else {
       std::cout << std::format("{}: command not found\n", input);
     }
