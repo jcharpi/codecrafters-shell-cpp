d32a4d504f07ebd9d2417da3d032d1f09eb42062
Josh Charpentier <81543042+jcharpi@users.noreply.github.com>
2026-05-24 22:57:38 -0500

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,15 @@
 #include <algorithm>
 #include <sstream>
 #include <unistd.h>
+#include <sys/wait.h>
 
 #ifdef _WIN32 
 constexpr char PATH_DELIMITER = ';';
 #else
 constexpr char PATH_DELIMITER = ':';
 #endif
+
+const std::vector<std::string> builtins = {"exit", "echo", "type"};
 
 bool isFileExecutable(const std::string& path) {
   return access(path.c_str(), X_OK) == 0;
@@ -32,11 +35,64 @@ std::string executableInPATH(const std::string& fileName) {
   return "";
 }
 
+void handleBuiltin(std::string cmd, std::string args) {
+  if (cmd == "exit") {
+    std::exit(EXIT_SUCCESS);
+  } else if (cmd == "echo") {
+    std::cout << args << "\n";
+  } else if (cmd == "type") {
+    if (std::find(builtins.begin(), builtins.end(), args) != builtins.end()) { // Builtin
+      std::cout << args << " is a shell builtin\n";
+    } else { // Executable
+      std::string executablePath = executableInPATH(args);
+      if (!executablePath.empty()) {
+        std::cout << args << " is " << executablePath << "\n";
+      } else {
+        std::cout << args << ": not found\n";
+      }
+    }
+  }
+}
+
+void handleExecutable(std::string cmd, std::string args, std::string file) {
+  // Split args into array
+  std::vector<std::string> argsList {cmd};
+  std::stringstream ss_args(args);
+  for (std::string arg; ss_args >> arg;) {
+    argsList.push_back(arg);
+  }
+
+  // Convert string array of args to char* array for C execvp method
+  std::vector<char*> argv;
+  argv.reserve(argsList.size() + 1); // +1 for the nullptr at the end
+  for (std::string& arg : argsList) {
+    argv.push_back(arg.data());
+  }
+  argv.push_back(nullptr);
+
+  // Stage 1: Fork shell process into two processes
+  pid_t pid = fork();
+  if (pid == -1) {
+    std::cerr << "handleExecutable(): fork failed\n";
+    return;
+  }
+
+  if (pid == 0) { // Child process
+    // Stage 2: Execute
+    execvp(file.c_str(), argv.data());
+    std::cerr << "handleExecutable(): execute failed\n";
+    std::exit(EXIT_FAILURE);
+  } else { // Main, parent process
+    // Stage 3: Wait for child to execute
+    if (waitpid(pid, nullptr, 0) == -1) {
+      std::cerr << "handleExecutable(): wait failed\n";
+    }
+  }
+}
+
 int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
-  const std::vector<std::string> builtins = {"exit", "echo", "type"};
 
   std::string input;
   while (true) {
@@ -45,23 +101,10 @@ int main() {
 
     std::string cmd = input.substr(0, input.find(" "));
     std::string args = input.size() > cmd.size() ? input.substr(cmd.size() + 1) : "";
-
-    if (cmd == "exit") {
-      break;
-    } else if (cmd == "echo") {
-      std::cout << args << "\n";
-    } else if (cmd == "type") {
-      // Builtin
-      if (std::find(builtins.begin(), builtins.end(), args) != builtins.end()) {
-        std::cout << args << " is a shell builtin\n";
-      } else { // Executable
-        std::string executablePath = executableInPATH(args);
-        if (!executablePath.empty()) {
-          std::cout << args << " is " << executablePath << "\n";
-        } else {
-          std::cout << args << ": not found\n";
-        }
-      }
+    if (std::find(builtins.begin(), builtins.end(), cmd) != builtins.end()) {
+      handleBuiltin(cmd, args);
+    } else if (std::string file = executableInPATH(cmd); !file.empty()) {
+      handleExecutable(cmd, args, file);
     } else { // Unrecognized
       std::cout << std::format("{}: command not found\n", cmd);
     }
